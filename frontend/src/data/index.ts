904646d77899b21c9232a62176e74c6f51a97dbd
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-11-04 10:25:01 -0600

    add: data index file
    This file contains all "dynamic" data that would be used by our frontend

---
+++ frontend/src/data/index.ts
@@ -0,0 +1,10 @@
+export const navigationItems = [
+  {
+    title: "Clasificador",
+    href: "#clasfier",
+  },
+  { title: "Características", href: "#features" },
+  { title: "Acerca de", href: "#about" },
+];
+
+export type NavigationItems = typeof navigationItems;
