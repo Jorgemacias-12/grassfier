900f32487fac5aed822112ff39b8fc771e1ef15c
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-11-04 11:43:45 -0600

    feat: implement footer

--- frontend/src/data/index.ts
+++ frontend/src/data/index.ts
@@ -25,3 +25,76 @@ export const heroFacts = [
 ];
 
 export type HeroFacts = typeof heroFacts;
+
+export const footerLinks = [
+  {
+    title: "Producto",
+    subLinks: [
+      {
+        label: "Caracteristicas",
+        href: "#",
+      },
+      {
+        label: "Precios",
+        href: "#",
+      },
+      {
+        label: "Documentación",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Empresa",
+    subLinks: [
+      {
+        label: "Acerca de",
+        href: "#",
+      },
+      {
+        label: "Blog",
+        href: "#",
+      },
+      {
+        label: "Contact",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Legal",
+    subLinks: [
+      {
+        label: "Privicidad",
+        href: "#",
+      },
+      {
+        label: "Términos",
+        href: "#",
+      },
+      {
+        label: "Cookies",
+        href: "#",
+      },
+    ],
+  },
+];
+
+export type FooterLinks = typeof footerLinks;
+
+export const footerSocialLinks = [
+  {
+    title: "X",
+    href: "#X",
+  },
+  {
+    title: "LinkedIn",
+    href: "#LinkedIn",
+  },
+  {
+    title: "GitHub",
+    href: "#GitHub",
+  },
+];
+
+export type FooterSocialLinks = typeof footerSocialLinks;
